b09e5d4f51ad9bc6a61e6d41c0e24069f6a2948f
Usuario <Usuario@CPD02-M14>
2024-03-12 11:45:47 -0300

    Adicionar arquivos de projeto.

---
+++ aprovacao/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace aprovacao
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Informe a média do aluno: ");
+             double media = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Informe o percentual de frequência do aluno: ");
+             int presenca = int.Parse(Console.ReadLine());
+
+            if (media >= 7 && presenca >= 75)
+            {
+                Console.WriteLine("O aluno está aprovado!");
+            }
+
+            else if (media < 7 && media >= 4 && presenca >= 75 || media>=7 && presenca<75)
+            {
+                Console.WriteLine("O aluno está de recuperação!");
+            }
+
+            else
+            {
+                Console.WriteLine("O aluno está retido.");
+            }
+            Console.ReadKey();
+        }
+    }
+}
